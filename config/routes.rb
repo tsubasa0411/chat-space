ab24b47321ada9bbd6be5615e119c87f20ef98ef
tsubasa <tonochi.tsu@gmail.com>
2019-10-03 21:49:17 +0900

    ログイン画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-root to: 'messages#index'
-
-
+    root to: 'messages#index' 
+  
+  # ルートパスの設定
+  # root 'groups#index'
+  resources :users, only: [:edit, :update, :index]
+  # ユーザー編集するためのパス
+  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :messages, only: [:index]
+    # グループ機能のルーティング
+  end
 end
