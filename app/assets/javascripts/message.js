5d69a66d933d59238960dbead8aa30ebae0f02be
tsubasa <tonochi.tsu@gmail.com>
2019-10-11 16:08:07 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,3 +53,31 @@ $('#new_message').on('subumit',function(e){
 
 });
 })
+// インクリメンタルサーチの記述↑
+
+// ↓自動更新
+var reloadMessages = function(){
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){ //今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。 
+    var last_message_id = $('.message:last').data("message-id");//dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+  $.ajax({
+    url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
+        type: 'get', //メソッドを指定
+        dataType: 'json', //データはjson形式
+        data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+  })
+  .done(function(messases){ //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
+    var insertHTML =""; //追加するHTMLの入れ物を作る
+    messases.forEach(function(messase){//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+      $(".messages").append(instertHTML);//メッセージを追加
+    })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+      })
+      .fall(function(){
+        alert("自動更新に失敗しました");//ダメだったらアラートを出す
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do #APIのコントローラを動かすための記述
+      resources :messages, only: :index, defaults: { format: 'json' } #defaultsオプションを利用して、このルーティングが来たらjson形式でレスポンスするよう指定。
+    end
   end
 end
