b478ca8b293c6c525976aaaeb721a2cdff03a2ea
tsubasa <tonochi.tsu@gmail.com>
2019-10-11 16:28:33 +0900

    コメントアウトを除去

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -60,21 +60,21 @@ var reloadMessages = function(){
   if (window.location.href.match(/\/groups\/\d+\/messages/)){ //今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。 
     var last_message_id = $('.message:last').data("message-id");//dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
   $.ajax({
-    url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
-        type: 'get', //メソッドを指定
-        dataType: 'json', //データはjson形式
-        data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+    url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} //取得したlast_message_id。またparamsとして渡すためlast_idとする。
   })
   .done(function(messases){ //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
-    var insertHTML =""; //追加するHTMLの入れ物を作る
+    var insertHTML =""; 
     messases.forEach(function(messase){//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-      insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+      insertHTML = buildHTML(message); 
       $(".messages").append(instertHTML);//メッセージを追加
     })
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
       })
       .fall(function(){
-        alert("自動更新に失敗しました");//ダメだったらアラートを出す
+        alert("自動更新に失敗しました");
       });
     }
   };
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # 未ログイン時はログインページにいく
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # config.action_view.automatically_disable_submit_tag = false 
-  # 非同期通信で最初のメッセージは発火するが、二回め以降も自動的に処理が発火するように定義
   protected
 
   def configure_permitted_parameters
