8d779b619b0caed2cdfd129718a630d1756d8fb8
tsubasa <tonochi.tsu@gmail.com>
2019-10-15 11:27:08 +0900

    変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,70 +1,85 @@
-$(function(){
+$(function() {
 
   function buildHTML(message){
-    var content = message.is_content_present ? `${message.content} ` : ''
-    var image = message.is_image_present ? `<img src='${message.image.url}'> ` : ''
-
-    var html = `<div class = "main-contents__body__list__message" data-id=${message.id}>
-                  <div class = "main-contents__body__list__message__name">
-                    ${message.user_name}
-                  </div>
-                  <div class = "main-contents__body__list__message__data">
-                    ${message.date}
-                  </div>
-                  <div class = "main-contents__body__list__message__body">
-                    ${content}
+  image = (message.image) ? `<img class="lower-message__image" src="${ message.image }">`: "";
+  var html =  `<div class="message" user_id= "${message.id}">
+                  <div class="upper-message">
+                        <div class="upper-message__user-name">
+                            ${ message.user_name }
+                        </div>
+                        <div class="upper-message__date">
+                            ${ message.time }
+                        </div>
+                    </div>
+                    <div class="lower-message">
+                    <p class="lower-message__content">
+                    ${ message.content }
+                    </p>
+                    </div>
                     ${image}
-                  </div>
                 </div>`
-    return html;
+      return html;
+
+    };
   }
-  //非同期通信
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.main-contents__body__list').append(html);
-      $(".main-contents__body").animate({scrollTop:$('.main-contents__body__list')[0].scrollHeight});
-      $('.new_message .message').val('');
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
-  // 自動更新
-    var interval = setInterval(function() {
-      if (location.href.match(/\/groups\/\d+\/messages/)){
-        var message_id = $('.main-contents__body__list__message').last().data('id');
-        $.ajax({
-          url: location.href,
-          type: "GET",
-          data: {id: message_id},
-          dataType: "json"
-        })
-        .done(function(data) {
-          data.forEach(function(message) {
-            var html = buildHTML(message);
-            $('.main-contents__body__list').append(html);
-            $(".main-contents__body").animate({scrollTop:$('.main-contents__body__list')[0].scrollHeight});
-            $('.new_message .message').val('');
-          })
-        })
-        .fail(function() {
-          alert('自動更新に失敗しました');
-        });
-      } else {
-          clearInterval(interval);
-        }
-    } , 5000 );
+
+$('#new_message').on('subumit',function(e){
+  e.preventDefault();
+  // イベントを止め、同期通信で送信されるのをやめる
+  var formData = new FormData(this);
+  // formから送信された内容を取得
+  var url = $(this).attr('action');
+  // formのアクションの属性を取得し、変数に代入
+$.ajax({//データベース送信
+  url: url,
+  type: "POST",
+  data: formData,
+  dataType: "json",
+  processData: false,
+  contentType: false
+})
+
+      .done(function(data){
+              var html = buildHTML(data);
+            $(".messages").append(html.animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+            $('.form__message').val('');
+            $('.form__submit').prop('disabled', false);
+
+})
+      .fail(function(){
+        alert('error');
+        $('.form__submit').prop('disabled', false);
+
+  });
+
 });
+
+
+
+ 
+    // ↓自動更新
+var reloadMessages = function(){
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){ //今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。 
+    var last_message_id = $('.message:last').data("message-id");//dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+  $.ajax({
+    url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
+        type: 'get', //メソッドを指定
+        dataType: 'json', //データはjson形式
+        data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+  })
+  .done(function(messases){ //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
+    var insertHTML =""; //追加するHTMLの入れ物を作る
+    messases.forEach(function(messase){//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+      $(".messages").append(instertHTML);//メッセージを追加
+    })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+      })
+      .fall(function(){
+        alert("自動更新に失敗しました");//ダメだったらアラートを出す
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+  });
+}); 
