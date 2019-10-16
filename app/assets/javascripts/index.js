2e2946c660a51625fee267872682bf1ad8265cfc
tsubasa <tonochi.tsu@gmail.com>
2019-10-16 17:24:15 +0900

    あ

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -62,7 +62,6 @@ $(document).on('turbolinks:load', function() {
 
     $(document).on("click", ".user-search-remove", function () {
       $(this).parent().remove();
-      $('#chat-group-users')[0].reset();
     });
   });
 });
